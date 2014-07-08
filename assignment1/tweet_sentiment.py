bdb0d197d40602d6e1d4482856e6b6a587032d69
Andre Ambrosio Boechat <boechat107@gmail.com>
2014-07-07 22:30:00 -0300

    Basic implementation to solve the problem 2: just single word
    * The dictionaries entries composed of more than one word aren't
    been used.

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -1,17 +1,35 @@
 import sys
+import json
+import string
 
-def hw():
-    print 'Hello, world!'
+def normalize_text(text):
+    return text.strip(string.punctuation).lower()
 
-def lines(fp):
-    print str(len(fp.readlines()))
+def build_dictionary(sent_file):
+    dic = {}
+    for line in sent_file:
+        words, score = line.split('\t')
+        dic[normalize_text(words)] = int(score)
+    return dic
+
+def compute_tweet_score(tweet_line, dic):
+    tweet_json = json.loads(tweet_line)
+    content = tweet_json.get('text')
+    score = 0
+    if content:
+        words = content.split()
+        for w in words:
+            w_score = dic.get(normalize_text(w))
+            if w_score:
+                score = score + w_score
+    return score
 
 def main():
     sent_file = open(sys.argv[1])
     tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+    sent_dic = build_dictionary(sent_file)
+    for tweet_line in tweet_file:
+        print compute_tweet_score(tweet_line, sent_dic)
 
 if __name__ == '__main__':
     main()
