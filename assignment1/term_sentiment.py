afa1fbaab84423ad8a73625afda53efcbd2c0eb5
Andre Ambrosio Boechat <boechat107@gmail.com>
2014-07-11 19:49:05 -0300

    Computation of the new words' score is finished

--- assignment1/term_sentiment.py
+++ assignment1/term_sentiment.py
@@ -30,18 +30,43 @@ def compute_tweet_score(words, dic):
             score = score + w_score
     return score
 
-def print_words_sentiment(content, dic, tweet_score):
-    pass
+def print_new_dictionary(new_dic):
+    for w, counters in new_dic.iteritems():
+        if counters[1] == 0:
+            print w, counters[0]
+        else:
+            print w, counters[0] / counters[1]
+
+
+## To calculate the sentiment of the terms that do not appear in the sentiment file,
+## we can divide the number of occurrences in positive tweets by the number of 
+## occurrences in negative tweets.
+## 
+## term_score = N of positive tweets 
+##              --------------------
+##              N of negative tweets
 
 def main():
     sent_file = open(sys.argv[1])
     tweet_file = open(sys.argv[2])
     sent_dic = build_dictionary(sent_file)
+    ## New dictionary with words that don't appear in the given sentiment file.
+    new_dic = {}
     for tweet_line in tweet_file:
         words = tweet_words(tweet_line)
         if words:
             tweet_score = compute_tweet_score(words, sent_dic)
-            print tweet_score
+            ## Counts positive and negative occurrences of some words.
+            for w in words:
+                if not sent_dic.has_key(w) or tweet_score != 0:
+                    np, nn = new_dic.get(w, (0,0))
+                    ## The tuple is composed of (N positive, N negative).
+                    if tweet_score > 0:
+                        np = np + 1
+                    else:
+                        nn = nn + 1
+                    new_dic[w] = (np, nn)
+    print_new_dictionary(new_dic)
 
 if __name__ == '__main__':
     main()
