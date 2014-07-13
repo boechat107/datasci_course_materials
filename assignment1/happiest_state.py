09846b6b2f224fe1580b9d9a7febc216aa9c6c47
Andre Ambrosio Boechat <boechat107@gmail.com>
2014-07-13 20:29:55 -0300

    Final solution for problem 5
    * Only the field place.fullname was used to locate the tweet.
    * First the State's abbreviation is checked; if it was not found,
    the State's name is checked.

--- assignment1/happiest_state.py
+++ assignment1/happiest_state.py
@@ -2,8 +2,13 @@ import sys
 import json
 import string
 
-## US States
-states = {
+def normalize_text(text):
+    return text.strip(string.punctuation + ' ').lower()
+
+def get_states_dic():
+    """Returns a dictionary of {state's abb : normalized state's name}.
+    """
+    states = { 
         'AK': 'Alaska',
         'AL': 'Alabama',
         'AR': 'Arkansas',
@@ -54,17 +59,18 @@ states = {
         'TN': 'Tennessee',
         'TX': 'Texas',
         'UT': 'Utah',
-        'VA': 'Virginia',
-        'VI': 'Virgin Islands',
-        'VT': 'Vermont',
-        'WA': 'Washington',
-        'WI': 'Wisconsin',
-        'WV': 'West Virginia',
-        'WY': 'Wyoming'
-}
-
-def normalize_text(text):
-    return text.strip(string.punctuation).lower()
+    'VA': 'Virginia',
+    'VI': 'Virgin Islands',
+    'VT': 'Vermont',
+    'WA': 'Washington',
+    'WI': 'Wisconsin',
+    'WV': 'West Virginia',
+    'WY': 'Wyoming' 
+    }
+    output = {}
+    for k in states.keys():
+        output[normalize_text(k)] = normalize_text(states[k])
+    return output
 
 def build_dictionary(sent_file):
     dic = {}
@@ -84,21 +90,47 @@ def compute_tweet_score(tweet_json, dic):
                 score = score + w_score
     return score
 
-def tweet_state(tweet_json):
+def tweet_state(tweet_json, state_dic):
     place = tweet_json.get('place')
-    if place:
-        print place.get('country')
+    if place and place.get('country_code') == 'US':
+        state_name, abb = place.get('full_name').split(',')
+        state_name = normalize_text(state_name)
+        abb = normalize_text(abb)
+        ## Checks the simplest way, the state's abbreviation.
+        if state_dic.has_key(abb):
+            return abb
+        ## Checks the city's name, which sometimes is the state's name (I 
+        ## don't know why it happens).
+        else:
+            for k, v in state_dic.iteritems():
+                if v == state_name:
+                    return k
+        return None
 
 def main():
     sent_file = open(sys.argv[1])
     tweet_file = open(sys.argv[2])
     sent_dic = build_dictionary(sent_file)
-    state_dic = {}
+    state_dic = get_states_dic()
+    state_score = {}
+    ## The score of each tweet is calculated and, if it's not zero, its State is
+    ## found. The calculated score is used to calculate the overall State score.
     for tweet_line in tweet_file:
         tweet_json = json.loads(tweet_line)
         tweet_score = compute_tweet_score(tweet_json, sent_dic)
         if tweet_score != 0:
-            state = tweet_state(tweet_json)
+            state = tweet_state(tweet_json, state_dic)
+            if state:
+                n = state_score.get(state, 0)
+                state_score[state] = n + tweet_score
+    ## Finding the State with the higher sentiment score.
+    maxv = - sys.maxint
+    maxk = None
+    for k, v in state_score.iteritems():
+        if v > maxv:
+            maxk = k
+    print maxk.upper()
+
 
 if __name__ == '__main__':
     main()
